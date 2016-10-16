92d93dc083c02fed8d78c5c9334b196539864323
Tijo <emailtijothomas@gmail.com>
2016-10-16 18:52:24 -0500

    Replaces data in helper.js with new info.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%", "Tijo Thomas");
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
