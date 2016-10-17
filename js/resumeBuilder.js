ffa8eead59d5f76592d4d24332c764f6686d9e0c
Tijo <emailtijothomas@gmail.com>
2016-10-17 18:13:40 -0500

    Adds image and cleans resumeBuilder.js to include only json.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,97 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-var formattedName = HTMLheaderName.replace("%data%", "Tijo Thomas");
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+// Below is what was done throughout the Data Types lessons, on Udacity.
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+
+// var formattedName = HTMLheaderName.replace("%data%", "Tijo Thomas");
+// var role = "Web Developer";
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+// var work = {};
+// work.position = "Course Developer";
+// work.employer = "Udacity";
+// work.years = 0.3;
+
+// $("#main").append(work["position"]);
+// $("#main").append(education.name);
+
+// $("#header").prepend(bio.bioPic);
+// $("#header").prepend(bio.skills);
+// $("#header").prepend(bio.welcomeMessage);
+// $("#header").prepend(bio.contacts);
+// $("#header").prepend(bio.role);
+// $("#header").prepend(bio.name);
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+var bio = {
+  "name": "Tijo Thomas",
+  "role": "Web Developer",
+  "contacts": {
+    "mobile": "555-555-5555",
+    "email": "john@example.com",
+    "github": "johndoe",
+    "twitter": "@johndoe",
+    "location": "San Francisco"
+  },
+  "welcomeMessage": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  "skills": ["awesomeness", "delivering things", "cryogenic sleep", "saving the universe"],
+  "bioPic": "images/me.jpg"
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "Nova Southeastern University",
+      "location": "Fort Lauderdale, FL",
+      "degree": "Masters",
+      "majors": ["CS"],
+      "dates": 2013,
+      "url": "http://example.com"
+    },
+    {
+      "name": "Eckerd College",
+      "location": "Saint Petersburg, FL",
+      "degree": "BA",
+      "majors": ["CS"],
+      "dates": 2003,
+      "url": "http://example.com"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "JavaScript Syntax",
+      "school": "Udacity",
+      "dates": 2014,
+      "url": "http://www.udacity.com"
+    }
+  ]
+}
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Planet Express",
+      "title": "Delivery Boy",
+      "dates": "January 3000 - Future",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis sit corporis eum architecto dolore sed labore quam officiis, vero soluta! Error quod, optio adipisci nemo, molestias ratione suscipit itaque laborum."
+    },
+    {
+      "employer": "Panucci's Pizza",
+      "title": "Delivery Boy",
+      "dates": "1998 - December 31, 1999",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, tempora aliquid aperiam aspernatur corporis beatae vitae qui. Reprehenderit deserunt beatae corporis excepturi, et molestiae omnis dicta eaque animi porro consectetur."
+    }
+  ]
+}
+
+var projects = {
+  "projects": [
+    {
+      "title": "Sample Project 1",
+      "dates": "2014",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere doloremque obcaecati architecto ipsum numquam similique doloribus unde repudiandae saepe sit minima error, repellendus accusantium vel quam, nobis dolorem. Delectus, ullam.",
+      "images": [
+        "https://images-na.ssl-images-amazon.com/images/I/71ytmRdg0%2BL._SL1280_.jpg",
+        "https://s-media-cache-ak0.pinimg.com/originals/8d/67/01/8d6701ec5b19e54bac95d4346d00352c.jpg"
+      ]
+    }
+  ]
+}
